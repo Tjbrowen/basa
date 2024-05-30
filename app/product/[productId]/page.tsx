a329f95debcc65502bf55e26740dadb796f59fa1
Tjbrowen <tjbrowen@gmail.com>
2024-05-30 12:47:21 +0200

    new file:   app/components/Avatar.tsx 	new file:   app/components/Heading.tsx 	new file:   app/product/[productId]/ListRating.tsx 	modified:   app/product/[productId]/page.tsx 	modified:   package-lock.json 	modified:   package.json

--- app/product/[productId]/page.tsx
+++ app/product/[productId]/page.tsx
@@ -2,6 +2,7 @@ import Container from "@/app/components/Container";
 import { product } from "@/utils/product";
 import { dividerClasses } from "@mui/material";
 import ProductDetails from "./ProductDetails";
+import ListRating from "./ListRating";
 interface IPrams {
     productId?: string 
 }
@@ -10,6 +11,17 @@ const Product = ({params} : {params: IPrams}) => {
     return (  <div className="p-8">
         <Container>
             <ProductDetails product = {product}/>
+            <div>
+                <div>
+                    Add Rating
+                </div>
+                <div className="flex flex-col mt-20 gap-4">
+                    <ListRating product={product}/>
+                    <div className="">
+
+                    </div>
+                </div>
+            </div>
         </Container>
          </div>);
 }
