4e822fac91d4b847fa22955df337eddab6d08616
Tjbrowen <tjbrowen@gmail.com>
2024-06-12 14:34:57 +0200

    new file:   app/components/nav/CartCount.tsx 	modified:   app/components/nav/NavBar.tsx

--- app/components/nav/NavBar.tsx
+++ app/components/nav/NavBar.tsx
@@ -2,11 +2,14 @@ import Link from "next/link";
 import Container from "../Container";
 import { Redressed } from "next/font/google";
 import Image from "next/image";
+import CartCount from "./CartCount";
 
-const redressed = Redressed ({subsets: ['latin'], weight: ["400"]});
+const redressed = Redressed({ subsets: ["latin"], weight: ["400"] });
 
 const NavBar = () => {
-    return <div className="
+  return (
+    <div
+      className="
     
     sticky
     top-0
@@ -15,33 +18,37 @@ const NavBar = () => {
     z-30
     shadow-sm
     
-    ">
-   <div className="py-4 border-b-[1px]">
-<Container>
-    <div className="
+    "
+    >
+      <div className="py-4 border-b-[1px]">
+        <Container>
+          <div
+            className="
     flex
     items-center
     justify-between
     gap-3
     md:gap-0
     text-slate-200
-    ">
-<Link href="/" className={`${redressed.className} font-bold text-2xl`}>
-    <Image src="/logo.png" alt="Logo" width={80} height={"80"} />
-</Link>
+    "
+          >
+            <Link
+              href="/"
+              className={`${redressed.className} font-bold text-2xl`}
+            >
+              <Image src="/logo.png" alt="Logo" width={80} height={"80"} />
+            </Link>
 
-
-
-
-        <div className="hidden md:block">Search</div>
-        <div className="flex items-center gap-8 md:gap-12">
-        <div>CartCount</div>
-        <div>UserMenu</div>
-        </div>
+            <div className="hidden md:block">Search</div>
+            <div className="flex items-center gap-8 md:gap-12">
+              <CartCount />
+              <div>UserMenu</div>
+            </div>
+          </div>
+        </Container>
+      </div>
     </div>
-</Container>
-   </div>
-    </div>;
-}
- 
+  );
+};
+
 export default NavBar;
