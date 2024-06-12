61dc1a7fc6d000799fc0f9ea4afdbb7bde2c229d
Tjbrowen <tjbrowen@gmail.com>
2024-06-12 16:30:50 +0200

    new file:   app/components/FormWrap.tsx 	modified:   app/components/inputs/input.tsx 	new file:   app/register/RegisterForm.tsx 	new file:   app/register/page.tsx

---
+++ app/register/RegisterForm.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { useState } from "react";
+import Heading from "../components/Heading";
+import Input from "../components/inputs/input";
+
+import { FieldValues, SubmitErrorHandler, useForm } from "react-hook-form";
+
+const RegisterForm = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  return (
+    <>
+      <Heading title="Sign up" />
+      <hr className="bg-slate-300 w-full h-px" />
+      <Input
+        id="name"
+        label="Name"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="email"
+        label="Email"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="password"
+        label="Password"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+        type="password"
+      />
+    </>
+  );
+};
+
+export default RegisterForm;
