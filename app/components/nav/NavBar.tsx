ef161a9308fff9a83df9b32825c1d09e82318963
Tjbrowen <tjbrowen@gmail.com>
2024-06-15 17:27:41 +0200

    new file:   app/components/nav/MenuItem.tsx 	modified:   app/components/nav/NavBar.tsx 	new file:   app/components/nav/UserMenu.tsx

--- app/components/nav/NavBar.tsx
+++ app/components/nav/NavBar.tsx
@@ -3,6 +3,7 @@ import Container from "../Container";
 import { Redressed } from "next/font/google";
 import Image from "next/image";
 import CartCount from "./CartCount";
+import UserMenu from "./UserMenu";
 
 const redressed = Redressed({ subsets: ["latin"], weight: ["400"] });
 
@@ -42,7 +43,7 @@ const NavBar = () => {
             <div className="hidden md:block">Search</div>
             <div className="flex items-center gap-8 md:gap-12">
               <CartCount />
-              <div>UserMenu</div>
+              <UserMenu />
             </div>
           </div>
         </Container>
